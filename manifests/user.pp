8b25237458b1c083cecf3e229cafd0ef5174f0f7
Tim Rogers <tiroger@microsoft.com>
2013-10-23 11:36:48 -0700

    Correction to user creation

--- manifests/user.pp
+++ manifests/user.pp
@@ -3,9 +3,14 @@
 # This Class defines the creation of gitlab users
 #
 
-define gitlab::ssh_key ($keystring, $user_email) {
-
-  if $keystring =~ /^(?:(.*-.*)\s)?(.*)\s?(.*)?$/ {
+define gitlab::ssh_key (
+  $keystring,
+  $user_email,
+  $dbuser = hiera('gitlab::gitlab_dbuser'),
+  $dbname = hiera('gitlab::gitlab_dbname'),
+  $dbpwd = hiera('gitlab::gitlab_dbpwd'),
+) {
+    if $keystring =~ /^(?:(.*-.*)\s)?(.*)\s?(.*)?$/ {
     if $1 =~ /ssh-(?:rsa|dss)/ {
       $type = $1
     } else {
@@ -24,8 +29,8 @@ define gitlab::ssh_key ($keystring, $user_email) {
 
   $keystr = "${type} ${key} ${comment}"
   exec {"add-gitlab-ssh-key-${key}":
-    command => "/usr/bin/mysql -u${gitlab_dbuser} -p${gitlab_dbpwd} -D${gitlab_dbname} -N -B -e\"insert into ${gitlab_dbname}.keys (user_id, title, ${gitlab_dbname}.keys.key, fingerprint) values ((select id from users where email='${user_email}' limit 1), '${name}', '${keystr}', '${fingerprint}')\"",
-    unless  => "/bin/sh -c \"! return `/usr/bin/mysql -u${gitlab_dbuser} -p${gitlab_dbpwd} -D${gitlab_dbname} -N -B -e\"select count(id) from ${gitlab_dbname}.keys where ${gitlab_dbname}.keys.key like '${keystr}'\"`\"",
+    command => "/usr/bin/mysql -u${dbuser} -p${dbpwd} -D${dbname} -N -B -e\"insert into ${dbname}.keys (user_id, title, ${dbname}.keys.key, fingerprint, created_at, updated_at) values ((select id from users where email='${user_email}' limit 1), '${name}', '${keystr}', '${fingerprint}', Now(), Now())\"",
+    unless  => "/bin/sh -c \"! return `/usr/bin/mysql -u${dbuser} -p${dbpwd} -D${dbname} -N -B -e\"select count(id) from ${dbname}.keys where ${dbname}.keys.key like '${keystr}'\"`\"",
   }
   
 #  $type = regsubst( $keystring, '^(?:(.*-.*)\s)?(.*)\s?(.*)?$', '\1' ),
@@ -33,18 +38,25 @@ define gitlab::ssh_key ($keystring, $user_email) {
 #  $comment = regsubst( $keystring, '^(?:(.*-.*)\s)?(.*)\s?(.*)?$', '\3' ),
 }
 
-define gitlab::user ($email = $title, $username, $pass, $ssh_keys = []) {
-#  $keys = ($ssh_keys.each {|$index, $val| class gitlab::ssh_key{"key${index}": keystring => $val} })
-#  $keys = [],
-#  $ssh_keys.each {|$index, $val| gitlab::ssh_key{"key${index}": keystring => $val} }
+define gitlab::user (
+  $email = $title,
+  $username = $title,
+  $pass = hiera('gitlab::user::pass'),
+  $ssh_keys = [],
+  $dbuser = hiera('gitlab::gitlab_dbuser'),
+  $dbname = hiera('gitlab::gitlab_dbname'),
+  $dbpwd = hiera('gitlab::gitlab_dbpwd'),
+) {
   exec {"add-gitlab-user-${username}":
-    command => "/usr/bin/mysql -u${gitlab_dbuser} -p${gitlab_dbpwd} -D${gitlab_dbname} -N -B -e\"insert into users (email, encrypted_password, Sign_in_count, created_at, name, username, admin, projects_limit, can_create_group, can_create_team) values ('${email}', '${pass}', 1, NOW(), '${username}', '${username}', 1, 500, 1, 1)\"",
-    unless  => "/bin/sh -c \"! return `/usr/bin/mysql -u${gitlab_dbuser} -p${gitlab_dbpwd} -D${gitlab_dbname} -N -B -e\"select count(id) from users where email like '${email}'\"`\"",
+    command => "/usr/bin/mysql -u${dbuser} -p${dbpwd} -D${dbname} -N -B -e\"insert into users (email, encrypted_password, Sign_in_count, created_at, name, username, admin, projects_limit, can_create_group, can_create_team) values ('${email}', '${pass}', 1, NOW(), '${username}', '${username}', 1, 500, 1, 1)\"",
+    unless  => "/bin/sh -c \"! return `/usr/bin/mysql -u${dbuser} -p${dbpwd} -D${dbname} -N -B -e\"select count(id) from users where email like '${email}'\"`\"",
   }
   $keys = ($ssh_keys.each { |$index, $val|  gitlab::ssh_key{ "${title}-key-${index}": 
       keystring => $val, 
-      user_email => $email} } )
-  
+      user_email => $email,
+      require => Exec["add-gitlab-user-${username}"],
+      } } 
+  )
 }
 
 
