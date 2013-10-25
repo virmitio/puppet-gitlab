bd9041caf54c74256bf7dc8ab711150df56eea02
Tim Rogers <tiroger@microsoft.com>
2013-10-25 15:19:11 -0700

    additional changes for adding groups, assigning users to groups, and disabling users

--- manifests/user.pp
+++ manifests/user.pp
@@ -1,6 +1,6 @@
 # Class: gitlab::user
 #
-# This Class defines the creation of gitlab users
+# This Class defines the creation and assignment of gitlab users and groups
 #
 
 define gitlab::ssh_key (
@@ -32,10 +32,6 @@ define gitlab::ssh_key (
     command => "/usr/bin/mysql -u${dbuser} -p${dbpwd} -D${dbname} -N -B -e\"insert into ${dbname}.keys (user_id, title, ${dbname}.keys.key, fingerprint, created_at, updated_at) values ((select id from users where email='${user_email}' limit 1), '${name}', '${keystr}', '${fingerprint}', Now(), Now())\"",
     unless  => "/bin/sh -c \"! return `/usr/bin/mysql -u${dbuser} -p${dbpwd} -D${dbname} -N -B -e\"select count(id) from ${dbname}.keys where ${dbname}.keys.key like '${keystr}'\"`\"",
   }
-  
-#  $type = regsubst( $keystring, '^(?:(.*-.*)\s)?(.*)\s?(.*)?$', '\1' ),
-#  $key = regsubst( $keystring, '^(?:(.*-.*)\s)?(.*)\s?(.*)?$', '\2' ),
-#  $comment = regsubst( $keystring, '^(?:(.*-.*)\s)?(.*)\s?(.*)?$', '\3' ),
 }
 
 define gitlab::user (
@@ -49,11 +45,11 @@ define gitlab::user (
 ) {
   if $email =~ /^.*@.*\..*$/ {
     if $username =~ /^(.*)@.*$/ {
-      $username = $1
+      $user_name = $1
     }
     exec {"add-gitlab-user-${username}":
-      command => "/usr/bin/mysql -u${dbuser} -p${dbpwd} -D${dbname} -N -B -e\"insert into users (email, encrypted_password, Sign_in_count, created_at, name, username, admin, projects_limit, can_create_group, can_create_team) values ('${email}', '${pass}', 1, NOW(), '${username}', '${username}', 1, 500, 1, 1)\"",
-      unless  => "/bin/sh -c \"! return `/usr/bin/mysql -u${dbuser} -p${dbpwd} -D${dbname} -N -B -e\"select count(id) from users where (email like '${email}') or (username like '${username}')\"`\"",
+      command => "/usr/bin/mysql -u${dbuser} -p${dbpwd} -D${dbname} -N -B -e\"insert into users (email, encrypted_password, Sign_in_count, created_at, updated_at, name, username, admin, projects_limit, can_create_group, can_create_team, state) values ('${email}', '${pass}', 1, NOW(), NOW(), '${user_name}', '${user_name}', 1, 500, 1, 1, 'active')\"",
+      unless  => "/bin/sh -c \"! return `/usr/bin/mysql -u${dbuser} -p${dbpwd} -D${dbname} -N -B -e\"select count(id) from users where (email like '${email}') or (username like '${user_name}')\"`\"",
     }
     $keys = ($ssh_keys.each { |$index, $val|  gitlab::ssh_key{ "${title}-key-${index}": 
         keystring => $val, 
@@ -66,5 +62,51 @@ define gitlab::user (
   }
 }
 
+define gitlab::group (
+  $name = $title,
+  $owner = UNSET,
+  $dbuser = hiera('gitlab::gitlab_dbuser'),
+  $dbname = hiera('gitlab::gitlab_dbname'),
+  $dbpwd = hiera('gitlab::gitlab_dbpwd'),
+) {
+  if $owner == UNSET {
+    $owner_ = '%'
+  }
+  exec {"add-group-${name}":
+    command => "/usr/bin/mysql -u${dbuser} -p${dbpwd} -D${dbname} -N -B -e\"insert into namespaces (name, path, type created_at, updated_at, owner_id) values ('${name}', Lower('${name}'), 'Group', NOW(), NOW(), (select id from users where email like '${owner_}' order by id limit 1))\"",
+    unless  => "/bin/sh -c \"! return `/usr/bin/mysql -u${dbuser} -p${dbpwd} -D${dbname} -N -B -e\"select count(id) from namespaces where Lower(name) like Lower('${name}')\"`\"",
+  }
+}
 
+define gitlab::group_user (
+  $user_email,
+  $groupname,
+  $access = hiera('gitlab::group_user::access'), #acceptable values, lowest to highest:  10, 20, 30, 40, 50
+  $dbuser = hiera('gitlab::gitlab_dbuser'),
+  $dbname = hiera('gitlab::gitlab_dbname'),
+  $dbpwd = hiera('gitlab::gitlab_dbpwd'),
+) {
+  exec {"add-groupuser-${name}":
+    command => "/usr/bin/mysql -u${dbuser} -p${dbpwd} -D${dbname} -N -B -e\"insert into users_groups (group_id, user_id, group_access, created_at, updated_at, notification_level) values ((select id from namespaces where Lower(name) like Lower('${groupname}') limit 1), (select id from users where (email like '${user_email}') limit 1), ${access}, NOW(), NOW(), 3)\"",
+    unless  => "/bin/sh -c \"! return `/usr/bin/mysql -u${dbuser} -p${dbpwd} -D${dbname} -N -B -e\"select (select count(id) from namespaces where Lower(name) like Lower('${groupname}')) and (select count(id) from users where (email like '${user_email}'))\"`\"",
+  }
+}
 
+define gitlab::block_user (
+  $user_email = $title,
+) {
+    exec {"add-gitlab-user-${username}":
+      command => "/usr/bin/mysql -u${dbuser} -p${dbpwd} -D${dbname} -N -B -e\"update users set state='blocked' where email like '${user_email}'\"",
+      unless  => "/bin/sh -c \"! return `/usr/bin/mysql -u${dbuser} -p${dbpwd} -D${dbname} -N -B -e\"select count(id)=1 from users where (email like '${user_email}')\"`\"",
+    }
+}
+
+define gitlab::cripple_user (
+  $user_email = $title,
+) {
+    exec {"add-gitlab-user-${username}":
+      command => "/usr/bin/mysql -u${dbuser} -p${dbpwd} -D${dbname} -N -B -e\"update users set encrypted_password=NULL where email like '${user_email}'\"",
+      unless  => "/bin/sh -c \"! return `/usr/bin/mysql -u${dbuser} -p${dbpwd} -D${dbname} -N -B -e\"select count(id)=1 from users where (email like '${user_email}')\"`\"",
+      require => Gitlab::Block_user["${user_email}"],
+    }
+}
